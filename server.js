be83c51b557a4160d09d701429851f750a719937
shubhank hiremath <shubhankhiremath@gmail.com>
2025-10-18 17:05:36 +0530

    backend new repo

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Handle preflight requests
-app.options('*', cors());
+app.options('*', cors(corsOptions));
 
 // Parse cookies BEFORE other middleware
 app.use(cookieParser());
